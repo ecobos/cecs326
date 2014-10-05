c4f3b7e0e9ee7a15abc077d615f731e4f3a28f9a
Edgar Cobos <ecobos@edcobos.com>
2014-10-04 20:52:51 -0700

    Mostly working already

--- child.c
+++ child.c
@@ -1,5 +1,25 @@
-#import <stdio.h>
-int main(){
-	printf("Hello");
-	return 0;
+# include <stdio.h>
+# include <time.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <unistd.h>  
+
+int main(int argc, char *argv[]) { 
+	 	int a, b, sleeptime; 
+	 	pid_t pid;
+	 
+	 	if (argc != 5) { 
+	 		printf("\n Usage: %s <sleeptime(0,50)> <int A> <int B> <PID>\n", argv[0]); 
+		 	exit(1); 
+		 } 
+		
+	 	sleeptime = atoi(argv[1]);
+		a = atoi(argv[2]);
+		b = atoi(argv[3]);
+		pid = atoi(argv[4]);
+		sleeptime = rand() % sleeptime; 
+		sleep(sleeptime);
+		printf("I am child number <blah> with PID %d\n", pid );
+		exit(0);
+		
 }
--- parent.c
+++ parent.c
@@ -2,32 +2,40 @@
 # include <stdlib.h> 
 # include <sys/types.h> 
 # include <unistd.h> 
+# include <time.h>
 
 int main(int argc, char *argv[]) 
 { 
- 	int i, m, n, sleeptime, seed; 
- 	pid_t childpid; 
- 	if (argc !=3) { 
- 		printf("\n Usage: %s processes\n", argv[0]); 
+ 	int a, b, sleeptime, seed; 
+ 	pid_t childpid;
+ 
+ 	if (argc != 4) { 
+ 		printf("\n Usage: %s <sleeptime(0,50)> <int A> <int B>\n", argv[0]); 
 	 	exit(1); 
 	 } 
- 	n = atoi(argv[1]); m = 10; 
- 	childpid = 0; 
- 	for (i=0; i<n;i++) 
- 	if (childpid = fork()) break; 
- 	if (childpid == -1) { 
- 		perror ("\n The fork failed\n"); 
- 		exit(1); 
- 	} 
- 	printf("\n %d: process ID:%6ld parent ID:%6ld child ID:%6ld",i, (long)getpid(), (long)getppid(), (long)childpid); 
- 	seed = (int)(getpid() + childpid); 
-	srand(seed); 
-	 /* since each process has a different childpid, using the childpid 
-	 as the seed number will restart the random function. 
-	 Therefore, each process will have a different sleeptime 
-	 */ 
-	 sleeptime = rand()%m; 
-	 printf (" sleep = %d\n", sleeptime); 
-	 sleep(sleeptime); 
+	
+ 	sleeptime = atoi(argv[1]);
+	a = atoi(argv[2]);
+	b = atoi(argv[3]);
+	 
+ 	printf("I am parent process, the maximum sleep time is %d and the two numbers are %d and %d\n", sleeptime, a, b);
+	sleeptime = rand() % sleeptime; 
+	sleep(sleeptime);
+	
+ 	for (int i=0; i < 4;i++){
+		childpid = fork();
+		if (childpid == 0){
+			char pid[10]; // again, 12 is an arbitrary value
+			snprintf(pid, sizeof(pid), "%d", getpid());
+			execlp("/Users/ecobos/git/cecs326/child", "child", argv[1], argv[2], argv[3], pid , (char *) NULL); 
+			perror("\nExec failure in parent\n");
+			//printf("\nI am child %d\n", getpid());
+			exit(1);
+		} else if (childpid == -1) {
+			perror("\nFork failure\n");
+			exit(1);
+		}
+		printf("Forked child %d\n", childpid);
+	}		 
 	 exit(0); 
 } 
